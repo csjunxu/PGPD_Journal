79940cc5f1eed6089f42a37dfe9128ef35441b0f
XuJun <nankaimathxujun@gmail.com>
2016-12-23 10:35:34 +0800

    add

--- Denoiser_Trainer.m
+++ Denoiser_Trainer.m
@@ -3,10 +3,23 @@ Original_image_dir  =    '../20images/';
 fpath = fullfile(Original_image_dir, '*.png');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-% load model according to noise levels
-nSig = 50;
-[par, model]  =  Parameters_Setting( nSig );
 
+%%
+nSig = 50;
+% [par, model]  =  Parameters_Setting( nSig );
+load './model/PG_GMM_8x8_win15_nlsp10_delta0.002_cls33.mat';
+par.nSig = nSig/255;
+par.step = 3;       % the step of two neighbor patches
+par.IteNum = 4;  % the iteration number
+par.ps = ps;        % patch size
+par.nlsp = nlsp;  % number of non-local patches
+par.Win = win;   % size of window around the patch
+% dictionary and regularization parameter
+for i = 1:size(GMM_D,2)
+    par.D(:,:,i) = reshape(single(GMM_D(:, i)), size(GMM_S,1), size(GMM_S,1));
+end
+par.S = single(GMM_S);
+%%
 for c1 = 0.15
     par.c1 = c1;
     for delta = 0.06
@@ -21,7 +34,6 @@ for c1 = 0.15
                 par.I = single( imread(fullfile(Original_image_dir, im_dir(i).name)) )/255;
                 %         S = regexp(im_dir(i).name, '\.', 'split');
                 randn('seed',0);
-                par.nSig = nSig/255;
                 par.nim =   par.I + par.nSig*randn(size(par.I));
                 fprintf('%s :\n',im_dir(i).name);
                 fprintf('The initial value of PSNR = %2.4f, SSIM = %2.4f \n', csnr( par.nim*255, par.I*255, 0, 0 ),cal_ssim( par.nim*255, par.I*255, 0, 0 ));
@@ -39,7 +51,7 @@ for c1 = 0.15
             mPSNR=mean(PSNR);
             mSSIM=mean(SSIM);
             fprintf('The average PSNR = %2.4f, SSIM = %2.4f. \n', mPSNR,mSSIM);
-            name = sprintf('PGPD_nSig%d_c%2.2f_delta%2.2f_eta%2.2f.mat',nSig,c1,delta,eta);
+            name = sprintf('nSig%d_c%2.2f_delta%2.2f_eta%2.2f.mat',nSig,c1,delta,eta);
             save(name,'nSig','PSNR','SSIM','mPSNR','mSSIM','mT512','sT512','mT256','sT256');
         end
     end
