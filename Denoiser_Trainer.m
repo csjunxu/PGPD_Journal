c812e31c691acd2534d0c18840885aac126ff080
XuJun <nankaimathxujun@gmail.com>
2017-01-19 14:07:56 +0800

    add

--- Denoiser_Trainer.m
+++ Denoiser_Trainer.m
@@ -1,63 +1,51 @@
-clear;
-Original_image_dir  =    '../20images/';
-fpath = fullfile(Original_image_dir, '*.png');
-im_dir  = dir(fpath);
+function [GPSNR, GSSIM] = Denoiser_Trainer(model, par, nSig, IteNum, testcluster, c1, mGPSNR, mGSSIM)
+
+im_dir  = dir(par.fpath);
 im_num = length(im_dir);
 
-%%
-nSig = 50;
-[par, model]  =  Parameters_Setting( nSig );
-% eval(['load par_c1_' num2str(nSig) '.mat']);
-%%
-for IteNum = 1:1:4
-    par.IteNum = IteNum;
-    testcluster = 1;
-    while testcluster <= model.nmodels
-        par.testcluster = testcluster;
-        mGPSNR = 0;
-        mGSSIM = 0;
-        c1 = 0;
-        index = false;
-        while ~index
-            c1 = c1 + 0.02;
-            par.c1(testcluster, IteNum) = c1;
-            % record all the results in each iteration
-            GPSNR = [];
-            GSSIM = [];
-            for i = 1:im_num
-                par.image = i;
-                par.nSig = nSig/255;
-                par.I = single( imread(fullfile(Original_image_dir, im_dir(i).name)) )/255;
-                S = regexp(im_dir(i).name, '\.', 'split');
-                randn('seed',0);
-                par.nim =   par.I + par.nSig*randn(size(par.I));
-                fprintf('%s :\n',im_dir(i).name);
-                fprintf('The initial value of PSNR = %2.4f, SSIM = %2.4f \n', csnr( par.nim*255, par.I*255, 0, 0 ),cal_ssim( par.nim*255, par.I*255, 0, 0 ));
-                %%
-                [im_out,par]  =  PGPD_DenoiserLearning(par,model);
-                % calculate the PSNR
-                GPSNR = [GPSNR  csnr( im_out*255, par.I*255, 0, 0 )];
-                GSSIM =  [GSSIM cal_ssim( im_out*255, par.I*255, 0, 0 )];
-                %                                 imname = sprintf('PGPD_nSig%d_%s',nSig,im_dir(i).name);
-                %                                 imwrite(im_out,imname);
-                fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n',im_dir(i).name, csnr( im_out*255, par.I*255, 0, 0 ), cal_ssim( im_out*255, par.I*255, 0, 0 ) );
-            end
-            fprintf('Cluster %d:\n', par.testcluster);
-            mGPSNR = [mGPSNR mean(GPSNR)];
-            mGSSIM = [mGSSIM mean(GSSIM)];
-            mGPSNRend = mGPSNR(end);
-            mGSSIMend = mGSSIM(end);
-            fprintf('The average PSNR = %2.4f, SSIM = %2.4f. \n', mGPSNR(end), mGSSIM(end));
-            name = sprintf('nSig%d_IteNum%d_cluster%d_c%2.2f.mat',nSig,IteNum,testcluster,c1);
-            save(name,'nSig','GPSNR','GSSIM','mGPSNR','mGSSIM','mGPSNRend','mGSSIMend');
-            if abs(mGPSNR(end) - mGPSNR(end - 1))< 1e-5
-                name = sprintf('param_c1_%d.mat',nSig);
-                param_c1 = par.c1;
-                save(name,'param_c1');
-                index = true;
-                break;
-            end
-        end
-        testcluster = testcluster + 1;
+eval(['load param_c1_' num2str(nSig) '.mat']);
+par.c1 = param_c1;
+par.testcluster = testcluster;
+
+index = false;
+while ~index
+    c1 = c1 + 0.02;
+    par.c1(testcluster, IteNum) = c1;
+    fprintf('IteNum = %d, testcluster = %d, c1 = %2.2f. \n', IteNum, testcluster, c1);
+    % record all the results in each iteration
+    GPSNR = [];
+    GSSIM = [];
+    for i = 1:im_num
+        par.image = i;
+        par.nSig = nSig/255;
+        par.I = double( imread(fullfile(par.Original_image_dir, im_dir(i).name)) )/255;
+        %         S = regexp(im_dir(i).name, '\.', 'split');
+        randn('seed',0);
+        par.nim = par.I + par.nSig*randn(size(par.I));
+        fprintf('%s :\n',im_dir(i).name);
+        fprintf('The initial value of PSNR = %2.4f, SSIM = %2.4f \n', csnr( par.nim*255, par.I*255, 0, 0 ),cal_ssim( par.nim*255, par.I*255, 0, 0 ));
+        %%
+        [im_out,par]  =  PGPD_DenoiserLearning(par,model);
+        %% calculate the PSNR
+        GPSNR = [GPSNR  csnr( im_out*255, par.I*255, 0, 0 )];
+        GSSIM =  [GSSIM cal_ssim( im_out*255, par.I*255, 0, 0 )];
+        % imname = sprintf('PGPD_nSig%d_%s',nSig,im_dir(i).name);
+        % imwrite(im_out,imname);
+        fprintf('%s : PSNR = %2.4f, SSIM = %2.4f \n', im_dir(i).name, csnr( im_out*255, par.I*255, 0, 0 ), cal_ssim( im_out*255, par.I*255, 0, 0 ) );
+    end
+    fprintf('Cluster %d:\n', par.testcluster);
+    mGPSNR = [mGPSNR mean(GPSNR)];
+    mGSSIM = [mGSSIM mean(GSSIM)];
+    mGPSNRend = mGPSNR(end);
+    mGSSIMend = mGSSIM(end);
+    fprintf('The average PSNR = %2.4f, SSIM = %2.4f. \n', mGPSNR(end), mGSSIM(end));
+    name = sprintf('results/%s_nSig%d_IteNum%d_cluster%d_c%2.2f.mat',par.TD, nSig, IteNum, testcluster, c1);
+    save(name, 'nSig', 'GPSNR', 'GSSIM', 'mGPSNR', 'mGSSIM', 'mGPSNRend', 'mGSSIMend');
+    if abs(mGPSNR(end) - mGPSNR(end - 1))< 1e-5
+        name = sprintf('%s_param_c1_%d.mat', par.TD, nSig);
+        param_c1 = par.c1;
+        save(name, 'param_c1');
+        index = true;
+        break;
     end
 end
---
+++ ParameterTuner.m
@@ -0,0 +1,41 @@
+clear;
+Original_image_dir  =    '../20images/';
+fpath = fullfile(Original_image_dir, '*.png');
+TD = regexp(Original_image_dir, '/', 'split');
+%%
+nSig = 50;
+[par, model]  =  Parameters_Setting( nSig );
+par.Original_image_dir = Original_image_dir;
+par.fpath = fpath;
+par.TD = TD{end-1};
+%%
+resultpath = dir('results/*.mat');
+resultlist={resultpath.name}';
+%%
+for IteNum = 1:1:4
+    par.IteNum = IteNum;
+    testcluster = 1;
+    while testcluster <= model.nmodels
+        resultname = sprintf('nSig%d_IteNum%d_cluster%d_', nSig, IteNum, testcluster);
+        resultexist = regexp(resultlist, resultname);
+        resultexist = ~cellfun('isempty', resultexist);
+        if sum(resultexist)
+            pos = find(resultexist==1);
+            eval(['load results/' resultlist{pos}]);
+            if abs(mGPSNR(end) - mGPSNR(end - 1))< 1e-5
+                testcluster = testcluster + 1;
+                continue;
+            else
+                S = regexp(resultlist{pos}, '_', 'split');
+                c1 = str2double(S{end}(2:5));
+                Denoiser_Trainer(model, par, nSig, IteNum, testcluster, c1, mGPSNR, mGSSIM);
+            end
+        else
+            mGPSNR = 0;
+            mGSSIM = 0;
+            c1 = 0;
+            Denoiser_Trainer(model, par, nSig, IteNum, testcluster, c1, mGPSNR, mGSSIM);
+        end
+        testcluster = testcluster + 1;
+    end
+end
